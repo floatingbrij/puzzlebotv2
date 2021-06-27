e5b88d74ffc8d5d7d32c455669197a70a1e887e7
Brijesharun G <brijesharun@gmail.com>
2021-06-27 21:32:56 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,7 +53,7 @@ client.on("message",async function(message)
   if(!userdata)
   { 	if (talkedRecently.has(message.author.id)) return;
 
-		if(!client.guild.cache.get(`777607607019110479`).roles.cache.get(`829402829705052230`).members.cache.has(message.author.id)) 
+		if(!client.guilds.cache.get(`777607607019110479`).roles.cache.get(`829402829705052230`).members.cache.has(message.author.id)) 
         {
         const succ = new Discord.MessageEmbed()
 		.setColor('#0099ff')
